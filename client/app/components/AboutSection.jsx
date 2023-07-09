d99fc083704adb572237ab092c902687f2f8c448
justallyt <albertjunk2030@gmail.com>
2023-07-09 19:32:06 +0300

    Updates

--- client/app/components/AboutSection.jsx
+++ client/app/components/AboutSection.jsx
@@ -2,6 +2,9 @@ import Image from "next/image"
 import feature1 from "../../public/feature1.jpg"
 import feature2 from "../../public/feature2.jpg"
 import feature3 from "../../public/hero2.jpg"
+import workflow1 from "../../public/workflow1.jpg"
+import workflow2 from "../../public/workflow2.jpg"
+import workflow3 from "../../public/workflow3.jpg"
 import SimpleFaq from "./SimpleFaq"
 import { features } from "../data/features"
 const AboutSection = () => {
@@ -38,6 +41,49 @@ const AboutSection = () => {
                                               <div className="flex items-center">
                                                           <Image src={feature2} className=" object-cover   rounded-2xl" alt="Feature Image"/>
                                               </div>
+                                   </div>
+                         </div>
+
+                         <div className="pt-14 mt-14">
+                                   <div className="grid grid-cols-2 gap-10">
+                                             <div className="flex flex-col gap-5 items-center">
+                                                      <Image src={workflow1} className="object-cover  rounded-2xl" height={300} alt="Workflow Image" />
+                                                      <div className="grid grid-cols-2 gap-5">
+                                                                   <Image src={workflow2} className="object-cover max-h-[300px]  rounded-2xl"  alt="Workflow Image" />
+                                                                   <Image src={workflow3} className="object-cover max-h-[300px]  rounded-2xl" alt="Workflow Image" />
+                                                      </div>
+                                             </div>
+                                             <div className="flex items-center">
+                                                        <div>
+                                                                   <h4 className="text-indigo-500 font-semiboldish">Workflow</h4>
+                                                                   <h1 className="font-boldish text-5xl mt-3 mb-5 block">See how Lively can help your business</h1>
+                                                                   <p className="text-lg w-11/12 my-5">There are more quantifiable reasons to choose Lively to propel your business to the next level by enabling you seamlessly integrate our chat solution with other tools for a streamlined workflow throughout your customer's entire buying journey. </p>
+
+                                                                   <div className="grid grid-cols-2 gap-5 mb-8">
+                                                                             <div>
+                                                                                       <h2 className="text-indigo-500 text-4xl font-semiboldish mb-4 ">120k +</h2>
+                                                                                       <p className="text-slate-900">Successful Campaigns</p>
+                                                                             </div>
+                                                                             <div>
+                                                                                       <h2 className="text-indigo-500 text-4xl font-semiboldish mb-4 ">150k +</h2>
+                                                                                       <p className="text-slate-900">New Customer Daily</p>
+                                                                             </div>
+                                                                             <div>
+                                                                                       <h2 className="text-indigo-500 text-4xl font-semiboldish mb-4 ">130k +</h2>
+                                                                                       <p className="text-slate-900">Onboarded Businesses</p>
+                                                                             </div>
+                                                                             <div>
+                                                                                       <h2 className="text-indigo-500 text-4xl font-semiboldish mb-4 ">165k +</h2>
+                                                                                       <p className="text-slate-900">Positive User reviews</p>
+                                                                             </div>
+                                                                   </div>
+
+                                                                   <div className="bg-indigo-100 p-8 rounded-xl">
+                                                                           <h3 className="font-semiboldish text-xl mb-3">Solid Engagement</h3>
+                                                                           <p>Reach out to visitors proactively using personalized chatbot greetings. Turn website visits into sales opportunities.</p>
+                                                                   </div>
+                                                        </div>
+                                             </div>
                                    </div>
                          </div>
              </div>
